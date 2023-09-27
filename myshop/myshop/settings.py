710717d0da31e31cd30d2f556c10ceac4842086e
shakori999 <shakori999@gmail.com>
2023-09-27 21:25:13 +0300

    dep10

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["assad-ecommerce-api-fcdd9d584d93.herokuapp.com/", ".herokuapp.com", "localhost", "127.0.0.1"]
 
 # Application definition
 
@@ -85,7 +85,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myshop.myshop.wsgi.application'
+WSGI_APPLICATION = 'myshop.wsgi.application'
 
 
 # Database
