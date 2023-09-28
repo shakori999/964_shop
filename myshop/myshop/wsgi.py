0f9b8caf6aea6216316a81708d20b574f3d4e783
shakori999 <shakori999@gmail.com>
2023-09-28 20:09:22 +0300

    dep23

--- myshop/myshop/wsgi.py
+++ myshop/myshop/wsgi.py
@@ -6,12 +6,15 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
-
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
-from django.conf import settings
+
+# Add the directory containing your project to the Python path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
 
 application = get_wsgi_application()
+
