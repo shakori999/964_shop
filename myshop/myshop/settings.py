35562d5f340f583e6f957436857bafa5b8f1eeb5
shakori999 <shakori999@gmail.com>
2023-09-28 20:02:31 +0300

    dep21

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -9,24 +9,21 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import os
 import dj_database_url
+
 import django_heroku
 
 from pathlib import Path
 from environs import Env
+
+
 from django.utils.translation import gettext_lazy as _
 
 env = Env()
 env.read_env()
 
-django_heroku.settings(locals())
-
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -71,6 +68,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+django_heroku.settings(locals())
 ROOT_URLCONF = 'myshop.urls'
 
 TEMPLATES = [
@@ -143,13 +141,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR / "static"] # new
-#STATIC_ROOT = BASE_DIR / "staticfiles" # new
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
  # new
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
